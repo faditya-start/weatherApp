87ce7c237fe5f70c41ebc944d62036645fc0641a
Faditya <daffaaditya2607@gmail.com>
2025-04-02 22:33:41 +0700

    feat: Add geolocation service

---
+++ src/services/geolocationService.ts
@@ -0,0 +1,25 @@
+interface GeolocationPosition {
+  latitude: number;
+  longitude: number;
+}
+
+export const getCurrentPosition = (): Promise<GeolocationPosition> => {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject(new Error('Geolocation is not supported by your browser'));
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
+      },
+      (error) => {
+        reject(new Error(error.message));
+      }
+    );
+  });
+}; 
